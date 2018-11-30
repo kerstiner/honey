6fc815944fa4d73d7231c90c8098dd87ba540380
Kerstine Rodilla <kerstinerodilla@gmail.com>
2018-11-30 13:54:38 +0800

    added add button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,46 +1,49 @@
 $(document).ready(function(){
 
-  var checkAll = $("#checkAll");
-  var checkbox = $(".checkbox");
-  var deleteButton = $(".deleteButton");
+  var doneTask = $(".done-task");
+  var deleteButton = $(".delete-button");
   var tableRow = $("#tableRow");
   var table = $(".table");
-  var showTodo = $(".showTodo");
-  var buttonEdit = $(".buttonEdit");
+  var showTodo = $(".show-to-do");
+  var buttonEdit = $(".button-edit");
   var save = $(".save");
   var add = $(".add");
 
   save.hide();
 
   deleteButton.click(function(){
-      $(this).closest('tr').remove();
-      return false;
+    $(this).closest('tr').remove();
+    return false;
   });
 
   buttonEdit.click(function(){
-    $(this).parents(".tableRow").find(".buttonEdit").hide(); 
+    $(this).parents(".tableRow").find(".button-edit").hide(); 
     $(this).parents(".tableRow").find("span").attr("contenteditable", true);
     $(this).parents(".tableRow").find(".save").show();
   });
 
   save.click(function(){
     $(this).parents(".tableRow").find(".save").hide();
-    $(this).parents(".tableRow").find(".buttonEdit").show();
+    $(this).parents(".tableRow").find(".button-edit").show();
     $(this).parents(".tableRow").find("span").attr("contenteditable", false); 
   });
 
   add.click(function(){
     var task = $("#task").val();
     var result = "<tr><td><input class='checkbox' type='checkbox'></td>" +
-                "<td class='showTodo' id='showInput1'><span contenteditable='false'></span> " + task + "</td> " +
-                "<td><button class='buttonEdit btn btn-info'><span class='glyphicon glyphicon-edit'></span>edit</button> " +
+                "<td class='show-to-do' id='showInput1'><span contenteditable='false'></span> " + task + "</td> " +
+                "<td><button class='button-edit btn btn-info'><span class='glyphicon glyphicon-edit'></span>edit</button> " +
                 "<button class='save btn btn-info'><span class='glyphicon glyphicon-save'></span>save</button>| " +
-                "<button class='btn btn-info deleteButton'><span class='glyphicon glyphicon-delete'></span>delete</button></td></tr>";
+                "<button class='btn btn-info delete-button'><span class='glyphicon glyphicon-delete'></span>delete</button></td></tr>";
 
-    $("table tbody").before(result);
+    $("table tbody").prepend(result);
+    $("#task").val("");
   });
 
+  doneTask.click(function(){
+    console.log("test");
+    $(this).parents(".tableRow").find(".show-to-do").toggleClass("strike");
+  });
+    
 });
-  
-
 
